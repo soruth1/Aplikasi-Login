14790e89544b785f0ed39bf08e02fba1511ec6db
soruth1 <142303651+soruth1@users.noreply.github.com>
2023-08-15 17:25:18 +0700

    Update login-script.js
    Mengganti komentar sesuai dengan format JSDoc

--- assets/scripts/login-script.js
+++ assets/scripts/login-script.js
@@ -6,20 +6,56 @@
  */
 
 const loginFormElement = document.querySelector('#loginForm');
+/**
+*Membuat variabel inputEmailElement untuk tampilan input email
+*@constant {HTMLElement}
+*/
 const inputEmailElement = document.querySelector('#inputEmail');
+
+
+/**
+*Membuat variabel inputPasswordElement untuk tampilan input password
+*@constant {HTMLElement}
+*/
 const inputPasswordElement = document.querySelector('#inputPassword');
 
+
+/**
+* Membuat variabel expectedEmail untuk menyimpan informasi email
+*@constant {string}
+*/
 const expectedEmail = 'admin@dicoding.com';
+
+
+/**
+* Membuat variabel expectedPassword untuk menyimpan informasi email
+*@constant {string}
+*/
 const expectedPassword = 'superpassword';
 
+
+/*Menambahkan aksi klik button. */
 loginFormElement.addEventListener('submit', function(event) {
   event.preventDefault();
 
+  /**
+  *Membuat variabel email untuk menyimapn nilai email yang didapatkan pada saat button ditekan
+  *@constant{string}
+  */
   const email = inputEmailElement.value;
+
+  /**
+  *Membuat variabel password untuk menyimapn nilai password yang didapatkan pada saat button ditekan
+  *@constant{string}
+  */
   const password = inputPasswordElement.value;
+
+  /*Memasukan bahwa nilai email dan password sesuai*/
   if (email == expectedEmail && password == expectedPassword) {
+    /*Jika sesuai move to Home*/
     goToHome();
   } else {
+    /*Jika tidak sesuai, tampilkan pop up notifikasi input salah*/
     showPopUp();
   }
 });
